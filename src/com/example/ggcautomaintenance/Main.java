71cdc2740e0d5ac6924c94cfb76cfa69a235aa69
santoine <santoine@ggc.edu>
2013-03-15 16:15:30 -0400

    Here it is.

--- src/com/example/ggcautomaintenance/Main.java
+++ src/com/example/ggcautomaintenance/Main.java
@@ -2,14 +2,36 @@ package com.example.ggcautomaintenance;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
+	
+	private Button mGetHelpButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mGetHelpButton = (Button) findViewById(R.id.button1);
+		
+		mGetHelpButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// do something
+				AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
+				builder.setTitle("Help!")
+				    .setMessage("This is the help text")
+				    .setNeutralButton("OK", null);
+
+				AlertDialog dialog = builder.create();
+				dialog.show();
+			}
+		});
 	}
 
 	@Override
@@ -17,6 +39,22 @@ public class Main extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+}
+	
+	
+	public void getCurrentMileage() {
+		
 	}
-
+	
+	public void setCurrentMileage() {
+		
+	}
+	
+	public void setFillupMileage() {
+		
+	}
+	
+	public void getNextMaintenance() {
+		
+	}
 }
