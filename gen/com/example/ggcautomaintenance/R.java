048254da96ad172504d283074a1e1f1e02357552
santoine <santoine@ggc.edu>
2013-03-15 18:44:15 -0400

    Fixed onClick method by adding @Override as Nick suggested.

--- gen/com/example/ggcautomaintenance/R.java
+++ gen/com/example/ggcautomaintenance/R.java
@@ -29,9 +29,9 @@ public final class R {
         public static final int buttonShowMaint=0x7f080002;
         public static final int enterButton=0x7f080000;
         public static final int fillBox=0x7f080004;
-        public static final int helpButton=0x7f080006;
+        public static final int helpButton=0x7f080005;
         public static final int linearLayout1=0x7f080001;
-        public static final int textView1=0x7f080005;
+        public static final int textView1=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/ggcautomaintenance/Main.java
+++ src/com/example/ggcautomaintenance/Main.java
@@ -14,45 +14,43 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-		
-	 public void helpMessage(View view)
-	 {
-	  Button button = (Button) findViewById(R.id.helpButton);
-	  button.setOnClickListener(new View.OnClickListener() 
-	  {    
-	   public void onClick(View v) 
-	   {        // Do something in response to button click    
-	    AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
-	    builder.setTitle("Help!")
-	       .setMessage("This is the help text")
-	       .setNeutralButton("OK", null);
-	    AlertDialog dialog = builder.create();
-	    dialog.show();
-	   }
-	  });  
-	 } 
+
+	public void helpMessage(View view)
+	{
+		Button button = (Button) findViewById(R.id.helpButton);
+		button.setOnClickListener(new View.OnClickListener() {    
+			@Override
+			public void onClick(View v) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
+				builder.setTitle("Help!")
+				.setMessage("This is the help text")
+				.setNeutralButton("OK", null);
+				AlertDialog dialog = builder.create();
+				dialog.show();
+			}
+		});  
+	} 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
-}
-	
-	
+	}
+
 	public void getCurrentMileage() {
-		
+
 	}
-	
+
 	public void setCurrentMileage() {
-		
+
 	}
-	
+
 	public void setFillupMileage() {
-		
+
 	}
-	
+
 	public void getNextMaintenance() {
-		
+
 	}
 }
