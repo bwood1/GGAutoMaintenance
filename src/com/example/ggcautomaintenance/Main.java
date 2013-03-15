39f43c85d95d13ae935313fd378d20c10db10952
santoine <santoine@ggc.edu>
2013-03-15 16:42:42 -0400

    Latest files with Nick's changes. Has working help popup

--- src/com/example/ggcautomaintenance/Main.java
+++ src/com/example/ggcautomaintenance/Main.java
@@ -8,31 +8,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class Main extends Activity {
-	
-	private Button mGetHelpButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-		mGetHelpButton = (Button) findViewById(R.id.button1);
-		
-		mGetHelpButton.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// do something
-				AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
-				builder.setTitle("Help!")
-				    .setMessage("This is the help text")
-				    .setNeutralButton("OK", null);
-
-				AlertDialog dialog = builder.create();
-				dialog.show();
-			}
-		});
 	}
+		
+	 public void helpMessage(View view)
+	 {
+	  Button button = (Button) findViewById(R.id.helpButton);
+	  button.setOnClickListener(new View.OnClickListener() 
+	  {    
+	   public void onClick(View v) 
+	   {        // Do something in response to button click    
+	    AlertDialog.Builder builder = new AlertDialog.Builder(Main.this);
+	    builder.setTitle("Help!")
+	       .setMessage("This is the help text")
+	       .setNeutralButton("OK", null);
+	    AlertDialog dialog = builder.create();
+	    dialog.show();
+	   }
+	  });  
+	 } 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
