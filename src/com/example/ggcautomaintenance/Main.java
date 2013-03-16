f93b3a1fcc48e9a12a6ad2db8998435efe6f700e
santoine <santoine@ggc.edu>
2013-03-16 17:52:45 -0400

    Nick incorporated the odometer.java, odometerspinner.java and a xml files

--- src/com/example/ggcautomaintenance/Main.java
+++ src/com/example/ggcautomaintenance/Main.java
@@ -16,10 +16,22 @@ public class Main extends Activity {
 			"* Select Calculate MPG to go to the Calculator Screen. \n" +
 			"* Select Show Maintenance to go to the Maintenance Items List screen.";
 
+	private static final String KEY_VALUE = "OdometerValue";
+	private Odometer mOdometer;
+	private int mOdometerValue;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mOdometer = (Odometer) findViewById(R.id.odometer);
+		
+	    if(savedInstanceState != null)
+	    {
+	        mOdometerValue = savedInstanceState.getInt(KEY_VALUE);
+	        mOdometer.setValue(mOdometerValue);
+	    }
 	}
 	
 	public void mpgButton(View view)		// unable to get this to work to call MPG Calculator Activity
@@ -57,6 +69,14 @@ public class Main extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	protected void onSaveInstanceState(Bundle outState)
+	{
+	    super.onSaveInstanceState(outState);
+	    
+	    mOdometerValue = mOdometer.getValue();	
+	    outState.putInt(KEY_VALUE, mOdometerValue);
 	}
 
 	public void getCurrentMileage() {
