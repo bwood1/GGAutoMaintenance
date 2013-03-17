1cd68748e9463dcaf8bc52025b17f233e382b918
Brandon <Brandon@Brandon-Desktop>
2013-03-16 22:15:41 -0400

    Created stubs for MaintRecordTableDataSource and MPGTableDataSource. You guys should now have all of the method names that you need to do your stuff. Let me know if something is missing.

--- src/com/example/ggcautomaintenance/MPGTableDataSource.java
+++ src/com/example/ggcautomaintenance/MPGTableDataSource.java
@@ -1,5 +1,63 @@
 package com.example.ggcautomaintenance;
 
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 public class MPGTableDataSource {
+	
+	//Database fields
+			private SQLiteDatabase database;
+			private MPGTableHelper mpgDbHelper;
+			private String[] allColumns = {
+								MPGTableHelper.COLUMN_FILL_NUMBER,
+								MPGTableHelper.COLUMN_CAR_NAME,
+								MPGTableHelper.COLUMN_ODOMETER,
+								MPGTableHelper.COLUMN_GALLONS,
+								MPGTableHelper.COLUMN_COST_PER_GALLON};
+			
+			//Constructor
+			public MPGTableDataSource (Context context) {
+				mpgDbHelper = new MPGTableHelper(context);
+			}
+			
+			//Open database
+			public void open() throws SQLException {
+				database = mpgDbHelper.getWritableDatabase();
+			}
+
+			//Close database
+			public void close() {
+				mpgDbHelper.close();
+			}
+			
+			//Get old MPG
+			public int getOldMileage(int oldFillNum) {
+				oldFillNum = 1;
+				int prevMileage = 0;
+				//TODO stuff to get the previous fill up mileage
+				return prevMileage;
+			}
+			
+			//Get current mileage
+			public int getCurrentMileage() {
+				int newMileage = 0;
+				//TODO stuff to get the current fill up mileage from the car table
+				return newMileage;
+			}
+			
+			//add a new mpg to the table
+			public MPG addMPG(int fillNumber, String carName, int oldOdomter, 
+									int gallons, double costPerGallon) {
+				//TODO stuff to add a new fill up to the database
+				return new MPG();
+			}
+			
+			//will update the old MPG with the new data
+			public MPG updateMPG(int fillNumber, String carName, int oldOdometer,
+										int gallons, double costPerGallon) {
+				//TODO stuff to update the MPG entry in the database
+				return new MPG();
+			}
 
 }
--- src/com/example/ggcautomaintenance/MaintRecordTableDataSource.java
+++ src/com/example/ggcautomaintenance/MaintRecordTableDataSource.java
@@ -1,5 +1,60 @@
 package com.example.ggcautomaintenance;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 public class MaintRecordTableDataSource {
+	
+	//Database fields
+		private SQLiteDatabase database;
+		private MaintRecordTableHelper maintRecordDbHelper;
+		private String[] allColumns = {
+					MaintRecordTableHelper.COLUMN_MAINT_RECORD_ID,
+					MaintRecordTableHelper.COLUMN_MAINT_COMPLETE_DATE,
+					MaintRecordTableHelper.COLUMN_CAR_NAME,
+					MaintRecordTableHelper.COLUMN_MAINT_ID,
+					MaintRecordTableHelper.COLUMN_ODOMETER,
+					MaintRecordTableHelper.COLUMN_COST};
+		
+		//Constructor
+		public MaintRecordTableDataSource (Context context) {
+			maintRecordDbHelper = new MaintRecordTableHelper(context);
+		}
+		
+		//Open database
+		public void open() throws SQLException {
+			database = maintRecordDbHelper.getWritableDatabase();
+		}
+
+		//Close database
+		public void close() {
+			maintRecordDbHelper.close();
+		}
+
+		//Create a new maint record
+		public MaintRecords createMaintRecord(int maintRecordId, String maintCompleteDate, String carName,
+												int maintId, int odometer, double cost) {
+			//TODO stuff to add a new car to the database
+			return new MaintRecords();
+		}
+		
+		//Get a single maint record from the database
+		public MaintRecords getMaintRecord(String maintRecordId /*maint record to pull*/) {
+			//TODO stuff to get a single maint record from the database
+			return null;
+		}
+		
+		//get all maint records from the database and put into an array list
+		public List<MaintRecords> getAllMaintRecords() {
+			List<MaintRecords> maintRecords = new ArrayList();
+			//TODO stuff to get all of the maint records from the database
+			return maintRecords;
+		}
 
 }
