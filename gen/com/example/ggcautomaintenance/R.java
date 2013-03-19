a952a43504cfef066c301cbb9fa5bb0181b1886d
santoine <santoine@ggc.edu>
2013-03-18 22:29:52 -0400

    Added color change on selection of a maintenance item in the list view.

--- gen/com/example/ggcautomaintenance/R.java
+++ gen/com/example/ggcautomaintenance/R.java
@@ -24,6 +24,9 @@ public final class R {
         public static final int helpicon=0x7f020000;
         public static final int ic_launcher=0x7f020001;
         public static final int roundback=0x7f020002;
+        public static final int row_background=0x7f020003;
+        public static final int row_background_default=0x7f020004;
+        public static final int row_background_pressed=0x7f020005;
     }
     public static final class id {
         public static final int MPGDisplay=0x7f080010;
